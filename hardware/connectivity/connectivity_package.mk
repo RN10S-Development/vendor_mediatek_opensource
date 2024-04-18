181390c368582d7e507798770396de5304c162f4
Stella Bloom <windowz414@gnuweeb.org>
2024-04-18 10:56:29 +0300

    [FOR REFERENCE] vendor: mtk: Adjust PRODUCT_PACKAGES definitions for rosemary
    * We don't enable all flags like begonia so there are quite a lot
      of packages that aren't getting built. This commit drops all
      packages that aren't being defined because of this. I assume this
      is safe to do so since we're using this tree for reference and not
      for production purposes.
    
    * I *could* try to adjust the code to upcoming Android releases
      instead of doing this and trying to import the compiled stuff as
      prebuilts, but it would only be more of an overkill and maintenance
      overhead than ease.
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- hardware/connectivity/connectivity_package.mk
+++ hardware/connectivity/connectivity_package.mk
@@ -1,9 +1,4 @@
 # Connectivity Packages
 PRODUCT_PACKAGES += \
-    libwapi \
-    wpa_supplicant.conf \
     libkeystore-wifi-hidl \
-    wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf \
-    libkeystore-engine-wifi-hidl \
-    android.hardware.wifi@1.0-service-lazy-mediatek
+    libkeystore-engine-wifi-hidl
